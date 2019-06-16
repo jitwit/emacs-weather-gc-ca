f40d8d59f32717c0e588228e35185ea54fa96af8
jitwit <jrn@bluefarm.ca>
2019-06-16 17:16:02 -0400

    found bug

--- weather-gc-ca.el
+++ weather-gc-ca.el
@@ -1,5 +1,4 @@
 ;;; weather-gc-ca.el --- Weather data from weather.gc.ca in Emacs -*- lexical-binding: t -*-
-
 (require 'url)
 (require 'xml)
 
@@ -8,12 +7,15 @@
   :prefix "weather-gc-ca-"
   :group 'Applications)
 
+
 (defcustom *weather-gc-ca-uri*
   "https://weather.gc.ca/rss/city/qc-147_e.xml"
   "weather.gc.ca rss uri for weather forecasts. default is for montreal.")
+
 (defvar *weather-gc-ca-feed* nil)
 (defvar *weather-gc-ca-current-conditions* nil)
 (defvar *weather-gc-ca-last-updated* nil)
+(defvar *weather-gc-ca-timer* nil)
 
 (defun weather-gc-ca-update ()
   "read the rss feed for montreal from weather-gc-ca"
@@ -26,6 +28,8 @@
 				     (car cs)
 				   '())))
 			     (xml-get-children parsed-xml 'entry))))
+	   ;; this works for mtl, doesn't seem to for vancouver...
+	   ;; i should rewrite this to actually look for prefix "Current Conditions:"
 	   (current-conditions (cadr (split-string (caddr (cadr entries)) ": "))))
       (setq *weather-gc-ca-feed* parsed-xml
 	    *weather-gc-ca-current-conditions* current-conditions
@@ -37,15 +41,25 @@
   (setq mode-line-misc-info
 	`(,(format "{%s}" *weather-gc-ca-current-conditions*))))
 
-(defvar *weather-gc-ca-timer*
-  (run-at-time (current-time) 900
-	       (lambda ()
-		 (weather-gc-ca-update)
-		 (weather-gc-ca-put-mode-line-misc-info))))
-
 (defun weather-gc-ca-cancel ()
-  (setq mode-line-misc-info '())
-  (cancel-timer *weather-gc-ca-timer*))
+  "Cancel the weather-gc-ca timer and clearn misc-info in mode line buffer."
+  (when *weather-gc-ca-timer*
+    (setq mode-line-misc-info '())
+    (cancel-timer *weather-gc-ca-timer*)))
+
+(defun weather-gc-ca-init ()
+  "Start weather-gc-ca timer now and update data every 30 minutes"
+  (weather-gc-ca-cancel)
+  (setq *weather-gc-ca-timer*
+	(run-at-time (current-time)
+		     1800
+		     (lambda ()
+		       (weather-gc-ca-update)
+		       (weather-gc-ca-put-mode-line-misc-info)))))
+
+(defun weather-gc-ca-new-uri (uri)
+  (setq *weather-gc-ca-uri* uri)
+  (weather-gc-ca-init))
 
 (provide 'weather-gc-ca)
 
